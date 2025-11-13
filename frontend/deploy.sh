041cdeca1556ae2cca8bc3bfce68a074bbc9e689
Tuienn <tuyenboy1234@gmail.com>
2025-11-13 21:20:44 +0700

    deploy nextjs-fe

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,11 +1,7 @@
-pm2 stop nextjs-fe
-cd /root/KmaERM/frontend || exit 1
-
 if [ "$1" = "reinstall" ]; then
     npm install
 fi
 
 npm run build
-HOST=0.0.0.0 pm2 start npm --name nextjs-fe -- start
 pm2 save
-pm2 restart nextjs-fe
+pm2 restart kmaerm-frontend
